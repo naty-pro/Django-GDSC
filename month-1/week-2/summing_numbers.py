f7fad7821ff0f1447d734783217c74bf0f9088b2
Natnael Asfaw <natnaelasfaw16@gmail.com>
2025-02-15 22:56:08 +0300

    Create summing_numbers.py

---
+++ month-1/week-2/summing_numbers.py
@@ -0,0 +1,9 @@
+# Function to sum numbers in a list
+def sum_numbers(numbers):
+    return sum(numbers)
+  
+# Example
+numbers_list = [10, 5, 8, 20, 3]
+
+# Sum of numbers
+print("Sum of numbers:", sum_numbers(numbers_list))
